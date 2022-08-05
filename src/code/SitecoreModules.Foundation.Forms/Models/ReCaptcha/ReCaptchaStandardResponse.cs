0c0e518f8d355b0619ea8672cb29f8298fd5cce2
Roberto Armas <roberto.armas@verndale.com>
2022-08-04 20:58:22 -0500

    ReCaptcha Standard Support

---
+++ src/code/SitecoreModules.Foundation.Forms/Models/ReCaptcha/ReCaptchaStandardResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SitecoreModules.Foundation.Forms.Models.ReCaptcha
+{
+    public class ReCaptchaStandardResponse
+    {
+        public DateTime challenge_ts { get; set; }
+        public float score { get; set; }
+        public List<string> ErrorCodes { get; set; }
+        public bool success { get; set; }
+        public string hostname { get; set; }
+    }
+}
